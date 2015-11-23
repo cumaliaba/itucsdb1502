db034bc4cadeb60856fce515d5211089d9eadb88
osman <ozsoyluo@itu.edu.tr>
2015-11-23 17:35:17 +0200

    team and teamroster class

---
+++ final4/models/team.py
@@ -0,0 +1,57 @@
+teamtable = ['id', 'name', 'coach_id']
+
+class Team:
+    def __init__(self, name, coach_id, id=None):
+        self.name = name
+        self.coach_id = coach_id
+        self.id=id
+		
+class Teams:
+    def __init__(self, conn, cur):
+        self.conn = conn
+        self.cur = cur
+        self.last_key = None
+
+    def add_team(self, team):
+        query = """INSERT INTO Teams (name, coach_id,id) 
+                                    values ('%s','%s','%s')""" % team.getAttrs()
+
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def delete_team(self, name):
+        query = "DELETE FROM Teams WHERE name='%s'" % name
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def update_team(self, key, team):
+        args = team.getAttrs() + (key,)
+        query = """UPDATE Teams SET name='%s', coach_id='%s', 
+                    id='%s'""" % args
+
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def get_team(self, name):
+        query = "SELECT * FROM Teams WHERE name='%s'" % name
+        self.cur.execute(query)
+        t = self.cur.fetchone()
+        if t:
+            print (t)
+            td = dict(zip(teamtable, t)) # team dict
+            team = Team(td['name'], td['coach_id'],td['id'])
+            print (team.age)
+            return team
+        return None
+
+    def get_teams(self):
+        query = "SELECT * FROM Teams;"
+        self.cur.execute(query)
+        teams = self.cur.fetchall()
+        teamlist = []
+        for t in Teams:
+            td = dict(zip(teamtable, t))
+            team = Team.fromDict(td)
+            teamlist.append(team)
+            
+        return teamlist
