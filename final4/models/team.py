1777542dafc104fed65153789216449c10151ab2
osman <ozsoyluo@itu.edu.tr>
2015-12-05 21:22:18 +0200

    player and team table completed

--- final4/models/team.py
+++ final4/models/team.py
@@ -1,11 +1,15 @@
-teamtable = ['id', 'name', 'coach_id']
+teamtable = ['id', 'name', 'coach_id', 'coach']
 
 class Team:
-    def __init__(self, name, coach_id, _id=None):
-	    self._id=_id        
-	    self.name = name
-	    self.coach_id = coach_id
-       		
+    def __init__(self, name, coach_id, coach=None, _id=None):
+        self._id = _id
+        self.name = name
+        self.coach_id = coach_id
+        self.coach = coach
+
+    def getAttrs(self):
+        return (dict(zip(teamtable, (self._id, self.name, self.coach_id, self.coach))))
+
 class Teams:
     def __init__(self, conn, cur):
         self.conn = conn
@@ -13,45 +17,84 @@ class Teams:
         self.last_key = None
 
     def add_team(self, team):
-        print("addteam",team)
+        print("addteam ", team)
         query = """INSERT INTO teams (name, coach_id) 
-                                    values (%s,%s)"""
-        self.cur.execute(query,(team.name,team.coach_id))
+                                    values (%s,%s)""" 
+
+        self.cur.execute(query, (team.name, team.coach_id))
         self.conn.commit()
 
     def delete_team(self, _id):
-        query = "DELETE FROM teams WHERE id=%s"""
-        self.cur.execute(query,(_id,))
+        query = """DELETE FROM teams WHERE id=%s"""
+        # variables should be in tuple object
+        self.cur.execute(query, (_id,))
         self.conn.commit()
 
-    def update_team(self,_id,new_team):
-        
-        query = """UPDATE teams SET name=%s, coach_id=%s, 
-                    id=%s""" 
-
-        self.cur.execute(query,(new_team.name,new_team.coach_id,_id))
+    def update_team(self, _id, new):
+        '''
+        new : team object
+        '''
+        print('update_team')
+        query = """UPDATE teams SET name=%s, coach_id=%s
+                    WHERE id=%s"""
+        self.cur.execute(query, (new.name, new.coach_id, _id))
         self.conn.commit()
 
-    def get_team(self, _id):
-        query = "SELECT * FROM teams WHERE id=%s" 
-        self.cur.execute(query,(_id))
-        t = self.cur.fetchone()
-        if t:
-            
-            td = dict(zip(teamtable, t)) # team dict
-            team = Team(td['name'], td['coach_id'],td['id'])
-            print (team.age)
+    def get_team(self,_id):
+        query = """SELECT teams.id, teams.name, countries.id, countries.name
+                        FROM teams,countries
+                        WHERE teams.id=%s AND countries.id=teams.coach_id
+                        ORDER BY teams.name
+                        """
+        self.cur.execute(query, (_id,))
+        l = self.cur.fetchone()
+        if l:
+            ld = dict(zip(teamtable, l[:len(teamtable)]))
+            team = Team(ld['name'], ld['coach_id'], ld['coach'], ld['id'])
             return team
         else:
             return None
 
-    def get_teams(self):
-        query = "SELECT * FROM teams;"
+    def get_teams(self, limit, offset):
+
+        query = """SELECT count(teams.id)
+                        FROM teams,countries WHERE countries.id=teams.coach_id
+                          """
         self.cur.execute(query)
+        total = self.cur.fetchone()[0]
+
+        query = """SELECT teams.id, teams.name, countries.id, countries.name 
+                        FROM teams,countries WHERE countries.id=teams.coach_id
+                          ORDER BY teams.name LIMIT %s OFFSET %s"""
+        self.cur.execute(query, (limit, offset))
         teams = self.cur.fetchall()
         teamlist = []
-        for t in teams:
-            td = dict(zip(teamtable, t))
-            team = Team(td['name'], td['coach_id'],td['id'])
+        for l in teams:
+            ld = dict(zip(teamtable, l))
+            team = Team(ld['name'], ld['coach_id'], ld['coach'],ld['id'])
             teamlist.append(team)
-        return teamlist
+        return teamlist, total
+
+    def get_teams_by(self, key, var, limit, offset):
+        skey = str(key) + '%'
+
+        query = """SELECT count(teams.id)
+                        FROM teams,countries WHERE teams.name LIKE %s AND 
+                            countries.id=teams.coach_id"""
+        self.cur.execute(query, (skey,))
+        total = self.cur.fetchone()[0]
+
+        query = """SELECT teams.id, teams.name, teams.coach_id, countries.name
+                        FROM teams,countries WHERE teams.name LIKE %s AND 
+                            countries.id=teams.coach_id ORDER BY teams.name
+                            LIMIT %s OFFSET %s"""
+        self.cur.execute(query, (skey,limit, offset))
+        teams = self.cur.fetchall()
+        print('teams:', teams)
+        teamlist = []
+        for l in teams:
+            ld = dict(zip(teamtable, l))
+            team = Team(ld['name'], ld['coach_id'], ld['coach'],ld['id'])
+            teamlist.append(team)
+        return teamlist, total
+
--- final4/views/player_view.py
+++ final4/views/player_view.py
@@ -4,29 +4,46 @@ import json
 from final4.config import app
 from final4.db_helper import getDb
 from final4.models import player
+from final4.models import country
 
 from flask import render_template
 from flask import request
 
 # player views
-@app.route('/players', methods=['DEL','GET', 'POST', 'PUT'])
+@app.route('/players', methods=['DEL','GET', 'POST'])
 def player_page():
     conn, cur = getDb()
     players = player.Players(conn, cur)
+    countries = country.Countries(conn, cur)
     print('PLAYERS PAGE')
     if request.method == 'GET':
-        p = players.get_players()
-        return render_template('players.html', playertable=player.playertable, players=p)
-    elif request.method == 'POST':
-        name = request.form['name']
-        country = request.form['country']
-        age = request.form['age']
-        playing_position = request.form['playing_position']
-        pl = player.Player(name, country,age,playing_position)
-        players.add_player(pl)
+        print ('GET REQUEST', request.args)
+        limit = int(request.args['limit']) if 'limit' in request.args else 10
+        page = int(request.args['page']) if 'page' in request.args else 0
         
-        p = players.get_player()
-        return render_template('players.html', playertable=player.playertable, platers=p)
+        offset = page*limit
+        print('page:',page,'limit',limit,'offset',offset)
+        orderby = request.args['orderby'] if 'orderby' in request.args else 'asc'
+        l, total = players.get_players(limit, offset)
+        c = countries.get_countries()
+        return render_template('players.html', playertable=player.playertable, players=l, countries=c, total=total, 
+                                limit=limit, page=page)
+    elif request.method == 'POST':
+        print('ADD PLAYER')
+        name = request.form['name']
+        age = request.form['age']
+        country_id = request.form['country']
+        limit = int(request.form['limit']) if 'limit' in request.form else 10
+        page = int(request.form['page']) if 'page' in request.form else 0
+        offset = page*limit
+        orderby = request.form['orderby'] if 'orderby' in request.form else 'asc'
+        lg = player.Player(name,age, country_id)
+        players.add_player(lg)
+        
+        l, total = players.get_players(limit, offset)
+        c = countries.get_countries()
+        return render_template('players.html', playertable=player.playertable, players=l, countries=c, total=total, 
+                                limit=limit, page=page)
 
     elif request.method == 'DEL':
         print ('DELETE REQUEST:PLAYERS PAGE')
@@ -47,44 +64,52 @@ def player_page():
             print (_id)
             players.delete_player(_id)
         return json.dumps({'status':'OK', 'idlist':idlist})
-        '''
-        try:
-            for _id in idlist:
-                print (_id)
-                leagues.delete_league(_id)
-            return json.dumps({'status':'OK', 'idlist':idlist})
-        except:
-            error = sys.exc_info()[0]
-            return json.dumps({'status':'FAILED', 'error':error})
-        '''
-        #return render_template('players.html', playertable=player.playertable, players=p)
 
-
-
-
-@app.route('/players/<pid>', methods=['GET','POST'])
-def player_from_id(pid):
+@app.route('/players/g/<lid>', methods=['GET','POST'])
+def player_from_id(lid):
     conn, cur = getDb()
     players = player.Players(conn, cur)
+    countries = country.Countries(conn, cur)
     
     if request.method == 'GET':
-        p = players.get_player(pid)
-        if p:
-            return json.dumps({'status':'OK', 'player':p.getAttrs()})
+        l= players.get_player(lid)
+        if l:
+            return json.dumps({'status':'OK', 'player':l.getAttrs()})
         else:
             return json.dumps({'status':'FAILED'})
     elif request.method == 'POST':
-        print("PUT METHOD REQUEST")
-        pid = request.form['id']
+        print("POST METHOD REQUEST")
+        lid = request.form['id']
         name = request.form['name']
-        country = request.form['country']
         age = request.form['age']
-        playing_position = request.form['playing_position']
-        print(pid, name, country)
-        pl = player.Player(name, country,age,playing_position)
-        players.update_player(pid, pl)
+        country_id = request.form['country']
+        # limit: number of result showing each page
+        # offset: selectedpage x limit
+        limit = int(request.form['limit']) if 'limit' in request.form else 10
+        page = int(request.form['page']) if 'page' in request.form else 0
+        offset = page*limit
+        orderby = request.form['orderby'] if 'orderby' in request.form else 'asc'
+        lg = player.Player(age, country_id)
+        players.update_player(lid, lg)
+        
+        l, total = players.get_players(limit, offset)
+        c = countries.get_countries()
+        return render_template('players.html', playertable=player.playertable, players=l, countries=c, total=total, 
+                                limit=limit, page=page)
 
-        p = players.get_players()
-        return render_template('players.html', playertable=player.playertable, players=p)
 
+@app.route('/players/s/<key>', methods=['GET','POST'])
+def search_player(key):
+    conn, cur = getDb()
+    players = player.Players(conn, cur)
+    countries = country.Countries(conn, cur)
 
+    limit = int(request.args['limit']) if 'limit' in request.args else 10
+    page = int(request.args['page']) if 'page' in request.args else 0
+    
+    offset = page*limit
+
+    result,total = players.get_players_by(key, 'name', limit, offset)
+    c = countries.get_countries()
+    return render_template('players.html', playertable=player.playertable, players=result, countries=c, total=total, 
+                            limit=limit, page=page)
