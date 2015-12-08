19b2c025366dbc14534550709f9735aa0011ce9d
Yusuf Temiz <myt.temiz@gmail.com>
2015-12-08 15:15:39 +0200

    league info page created and app.view function implemented

--- final4/views/league_view.py
+++ final4/views/league_view.py
@@ -17,34 +17,42 @@ def league_page():
     countries = country.Countries(conn, cur)
     print('LEAGUES PAGE')
     if request.method == 'GET':
+        # handle GET request
         print ('GET REQUEST', request.args)
         limit = int(request.args['limit']) if 'limit' in request.args else 10
         page = int(request.args['page']) if 'page' in request.args else 0
         
         offset = page*limit
         print('page:',page,'limit',limit,'offset',offset)
-        orderby = request.args['orderby'] if 'orderby' in request.args else 'asc'
+        sortby = request.args['sortby'] if 'sortby' in request.args else 'name'
+        order = request.args['order'] if 'order' in request.args else 'asc'
+ 
         l, total = leagues.get_leagues(limit, offset)
         c = countries.get_countries()
+        sortby={'attr':'name', 'property':'asc'}
         return render_template('leagues.html', leaguetable=league.leaguetable, leagues=l, countries=c, total=total, 
-                                limit=limit, page=page)
+                limit=limit, page=page, sortby=sortby)
     elif request.method == 'POST':
+        # handle POST request
         print('ADD LEAGUE')
         name = request.form['name']
         country_id = request.form['country']
         limit = int(request.form['limit']) if 'limit' in request.form else 10
         page = int(request.form['page']) if 'page' in request.form else 0
         offset = page*limit
-        orderby = request.form['orderby'] if 'orderby' in request.form else 'asc'
+        order = request.form['sortby'] if 'sortby' in request.form else 'name'
+        order = request.form['order'] if 'order' in request.form else 'asc'
         lg = league.League(name, country_id)
         leagues.add_league(lg)
         
         l, total = leagues.get_leagues(limit, offset)
         c = countries.get_countries()
+        sortby={'attr':'name', 'property':'asc'}
         return render_template('leagues.html', leaguetable=league.leaguetable, leagues=l, countries=c, total=total, 
-                                limit=limit, page=page)
+                                limit=limit, page=page, sortby=sortby)
 
     elif request.method == 'DEL':
+        # handle DEL request
         print ('DELETE REQUEST:LEAGUES PAGE')
         print (request.form)
         # concat json var with '[]' for calling array getted with request
@@ -71,12 +79,14 @@ def league_from_id(lid):
     countries = country.Countries(conn, cur)
     
     if request.method == 'GET':
+        # handle GET request
         l= leagues.get_league(lid)
         if l:
             return json.dumps({'status':'OK', 'league':l.getAttrs()})
         else:
             return json.dumps({'status':'FAILED'})
     elif request.method == 'POST':
+        # handle POST request
         print("POST METHOD REQUEST")
         lid = request.form['id']
         name = request.form['name']
@@ -86,14 +96,16 @@ def league_from_id(lid):
         limit = int(request.form['limit']) if 'limit' in request.form else 10
         page = int(request.form['page']) if 'page' in request.form else 0
         offset = page*limit
-        orderby = request.form['orderby'] if 'orderby' in request.form else 'asc'
+        order = request.form['sortby'] if 'sortby' in request.form else 'name'
+        order = request.form['order'] if 'order' in request.form else 'asc'
         lg = league.League(name, country_id)
         leagues.update_league(lid, lg)
         
         l, total = leagues.get_leagues(limit, offset)
         c = countries.get_countries()
+        sortby={'attr':'name', 'property':'asc'}
         return render_template('leagues.html', leaguetable=league.leaguetable, leagues=l, countries=c, total=total, 
-                                limit=limit, page=page)
+                                limit=limit, page=page, sortby=sortby)
 
 
 @app.route('/leagues/s/<key>', methods=['GET','POST'])
@@ -104,10 +116,35 @@ def search_league(key):
 
     limit = int(request.args['limit']) if 'limit' in request.args else 10
     page = int(request.args['page']) if 'page' in request.args else 0
+
+    sortby = request.args['sortby'] if 'sortby' in request.args else 'name'
+    order = request.args['order'] if 'order' in request.args else 'asc'
     
     offset = page*limit
 
     result,total = leagues.get_leagues_search_by('name', key,  limit, offset)
     c = countries.get_countries()
+    sortby={'attr':'name', 'property':'asc'}
     return render_template('leagues.html', leaguetable=league.leaguetable, leagues=result, countries=c, total=total, 
-                            limit=limit, page=page)
+                            limit=limit, page=page, sortby=sortby)
+
+@app.route('/leagues/league/<league_id>')
+def view_league(league_id):
+    conn, cur = getDb()
+
+    leagues = league.Leagues(conn, cur)
+
+    l = leagues.get_league(league_id)
+    if l is None:
+        # return not found error 
+        return render_template('error.html', err_code=404)
+
+    # else render country page with required args
+
+    #teams = teams.Teams(conn, cur)
+    #team_list, total = teams.get_teams_by('league_id', l.country_id)
+    #team_dict = {'teams':team_list,
+    #                'total': total
+    #                }
+    return render_template('league_page.html', league=l, 
+            team_dict=None)
