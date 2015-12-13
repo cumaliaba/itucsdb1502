f52e202293d05312fff379f65209f3f4b33bb1b4
buldur <abdullahbuldur@gmail.com>
2015-12-13 23:07:58 +0200

    bugs fixed

---
+++ sampledata/awards.sql
@@ -0,0 +1,9 @@
+INSERT INTO awards (name) VALUES('Rookie of the Year');
+INSERT INTO awards (name) VALUES('All-Star Game MVP');
+INSERT INTO awards (name) VALUES('Most Valuable Player');
+INSERT INTO awards (name) VALUES('NBA Finals Most Valuable Player');
+INSERT INTO awards (name) VALUES('Executive of the Year');
+INSERT INTO awards (name) VALUES('Defensive Player of the Year');
+INSERT INTO awards (name) VALUES('Sixth Man of the Year');
+INSERT INTO awards (name) VALUES('Most Improved Player');
+
